bb3bfa489337f7fc807c7f5fe65be9db5496e7c5
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2025-04-22 15:43:14 +0200

    add sendBundle and sendObject to OscClient

--- src/main/kotlin/eu/florian_fuhrmann/kotlin_osc/OscClient.kt
+++ src/main/kotlin/eu/florian_fuhrmann/kotlin_osc/OscClient.kt
@@ -1,6 +1,8 @@
 package eu.florian_fuhrmann.kotlin_osc
 
 import eu.florian_fuhrmann.kotlin_osc.packet.OscPacket
+import eu.florian_fuhrmann.kotlin_osc.packet.contents.OscObject
+import eu.florian_fuhrmann.kotlin_osc.packet.contents.bundle.OscBundle
 import eu.florian_fuhrmann.kotlin_osc.packet.contents.message.OscMessage
 import java.net.Socket
 
@@ -17,7 +19,13 @@ class OscClient(
 ) {
 
     /** Wraps the [oscMessage] in an [OscPacket] and sends it. */
-    fun sendMessage(oscMessage: OscMessage) = sendPacket(OscPacket(oscMessage))
+    fun sendMessage(oscMessage: OscMessage) = sendPacket(OscPacket(oscMessage)) // this is a bit redundant with #sendObject
+
+    /** Wraps the [oscBundle] in an [OscPacket] and sends it. */
+    fun sendBundle(oscBundle: OscBundle) = sendPacket(OscPacket(oscBundle)) // this is a bit redundant with #sendObject
+
+    /** Wraps the [oscObject] in an [OscPacket] and sends it. */
+    fun sendObject(oscObject: OscObject) = sendPacket(OscPacket(oscObject))
 
     /** Sends the given [OscPacket] to the socket. */
     fun sendPacket(oscPacket: OscPacket) {
